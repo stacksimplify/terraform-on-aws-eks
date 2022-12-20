d12397d6d7860ddf96b393bf37a9872f60065f73
vardhamani <manivardha96@gmail.com>
2022-12-20 17:39:55 +0530

    few vpc changes were made

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c3-vpc.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c3-vpc.tf
@@ -8,8 +8,9 @@ module "vpc" {
   name = "vpc-dev"
   cidr = "10.0.0.0/16"   
   azs                 = ["us-east-1a", "us-east-1b"]
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  #private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  #public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+
 
   # Database Subnets
   create_database_subnet_group = true
