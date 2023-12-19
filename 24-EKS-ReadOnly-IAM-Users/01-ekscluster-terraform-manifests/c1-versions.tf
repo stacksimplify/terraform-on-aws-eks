1c29f2a4c611bfe065a3f7140c63eb116fd1c308
Kalyan Reddy Daida <stacksimplify@gmail.com>
2023-12-19 10:56:29 +0530

    Welcome to StackSimplify by Kalyan Reddy Daida

--- 24-EKS-ReadOnly-IAM-Users/01-ekscluster-terraform-manifests/c1-versions.tf
+++ 24-EKS-ReadOnly-IAM-Users/01-ekscluster-terraform-manifests/c1-versions.tf
@@ -1,11 +1,11 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.6.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~> 4.12"
-      version = ">= 4.65"
+      #version = ">= 4.65"
+      version = ">= 5.31"      
      }
   }
   # Adding Backend as S3 for Remote State Storage
