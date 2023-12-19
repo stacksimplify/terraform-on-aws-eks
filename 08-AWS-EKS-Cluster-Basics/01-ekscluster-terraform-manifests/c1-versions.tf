58e3310b5ddff23dee2141108f5d7a471b02867b
Kalyan Reddy Daida <stacksimplify@gmail.com>
2023-12-19 09:12:30 +0530

    Welcome to StackSimplify by Kalyan Reddy Daida

--- 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c1-versions.tf
+++ 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c1-versions.tf
@@ -1,11 +1,11 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.6.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~> 3.63"
-      version = ">= 4.65"
+      #version = ">= 4.65"
+      version = ">= 5.31"
      }
   }
 }
--- 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c4-05-ec2bastion-instance.tf
+++ 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c4-05-ec2bastion-instance.tf
@@ -3,8 +3,8 @@
 module "ec2_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   #version = "3.3.0"
-  version = "5.0.0"  
-    
+  #version = "5.0.0"  
+  version = "5.5.0"      
   # insert the required variables here
   name                   = "${local.name}-BastionHost"
   ami                    = data.aws_ami.amzlinux2.id
